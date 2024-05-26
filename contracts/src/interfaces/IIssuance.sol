f1cdaa57dce4b417298b9cd65acc08dd2c958e8e
Patrick Kim <96800306+patrick-kimm@users.noreply.github.com>
2024-05-26 16:17:03 -0400

    Update IIssuance.sol

--- contracts/src/interfaces/IIssuance.sol
+++ contracts/src/interfaces/IIssuance.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.2;
 
+import {TokenInfo} from "src/Common.sol";
+
+interface IIssuance {
+    error IssuanceReentrant();
+    error IssuanceNoTokens();
+
+    function issue(uint256 amount) external;
+
+    function redeem(uint256 amount) external;
+}
