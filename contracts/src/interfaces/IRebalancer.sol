df9ee254acccc30467326538ae08269ef2b2c3bf
Patrick Kim <96800306+patrick-kimm@users.noreply.github.com>
2024-05-26 16:17:15 -0400

    Update IRebalancer.sol

--- contracts/src/interfaces/IRebalancer.sol
+++ contracts/src/interfaces/IRebalancer.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.2;
+import {TokenInfo} from "src/Common.sol";
 
+interface IRebalancer {
+    function rebalanceCallback(
+        TokenInfo[] calldata required,
+        TokenInfo[] calldata received
+    ) external;
+}
